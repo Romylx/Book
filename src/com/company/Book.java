079778baa9fd610ebcb3cd63456d92df417e9016
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2020-09-14 14:24:19 +0300

    Initial commit

---
+++ src/com/company/Book.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.lang.*;
+public class Book {
+    private String Title;
+    private String author;
+
+    public Book(String title, String author) {
+        setTitle(title);
+        setAuthor(author);
+
+    }
+    public void setTitle(String title) { this.Title = title; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getBookTitle() {return this.Title;}
+    public String getBookAuthor() {return this.author;}
+    public String toString() {
+        return "Book Title: " + "\nBall Author: " + this.author + "\n";
+    }
+
+}
---
+++ src/com/company/Book_Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.lang.*;
+public class Book_Test {
+    public static void main(String[] args) {
+        Book Book1 = new Book("The Master and Margarita", " Mikhail Bulgakov");
+        System.out.println("Book Title: " + Book1.getBookTitle());
+        System.out.println("Book Author: " + Book1.getBookAuthor() + "\n");
+    }
+}
